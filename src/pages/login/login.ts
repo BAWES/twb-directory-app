7d8bcd3f26291d06fe3e18baa726928e5153fbd4
Khalid <khalid@bawes.net>
2017-09-09 13:34:32 +0300

    Functional facebook login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _afAuth: AngularFireAuth) {
 
   }
 
   loginWithFacebook(){
     console.log("Logging in with facebook");
+    this._afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res => {
+      console.log(res);
+    });
   }
 
   logout(){
     console.log("Logging out.")
+    this._afAuth.auth.signOut();
   }
 
 }
