bcd146aa412e90f1eec9608733cb0521483110ce
Khalid <khalid@bawes.net>
2017-09-10 20:32:52 +0300

    Revert "Rename home to category page"
    This reverts commit ca29e4673717fb22a24933e785c10675f8497ce8.

---
+++ src/pages/category/category.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+import { CategoryFormPage } from '../forms/category-form/category-form';
+
+import { AuthService } from '../../providers/auth.service';
+
+@Component({
+  selector: 'page-category',
+  templateUrl: 'category.html'
+})
+export class CategoryPage {
+
+  public categories: FirebaseListObservable<any[]>;
+
+  constructor(
+    public navCtrl: NavController, 
+    public modalCtrl: ModalController,
+    public auth: AuthService,
+    db: AngularFireDatabase
+  ) {
+    this.categories = db.list('/categories');
+  }
+
+
+  /**
+   * Present create category page
+   */
+  createCategory(){
+    let modal = this.modalCtrl.create(CategoryFormPage, {
+      categories: this.categories
+    });
+    modal.present();
+  }
+
+}
