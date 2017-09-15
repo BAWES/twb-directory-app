9530d81b943507a5fb226efbf03a7fbdfd1bffdd
Khalid <khalid@bawes.net>
2017-09-15 16:15:24 +0300

    Functional file upload from mobile device

--- src/components/image-upload/image-upload.ts
+++ src/components/image-upload/image-upload.ts
@@ -1,6 +1,7 @@
 import { Component, Input, forwardRef, Renderer, ElementRef, ViewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Platform, AlertController, ActionSheetController } from 'ionic-angular';
+import { File as NativeFile, Entry, FileEntry }  from '@ionic-native/file';
 
 import { AuthService } from '../../providers/auth.service';
 import { CameraService } from '../../providers/camera.service';
@@ -54,7 +55,7 @@ export class ImageUploadComponent implements ControlValueAccessor {
     private _auth: AuthService,
     private _cameraService: CameraService,
     private _actionSheetCtrl: ActionSheetController,
-    private _alertCtrl: AlertController
+    private _nativeFile: NativeFile
     ) {
       // By Default, use the permanent bucket url
       this.bucketUrl = `/uploads/${this._auth.uid}`;
@@ -140,20 +141,66 @@ export class ImageUploadComponent implements ControlValueAccessor {
   /**
    * Upload the selected file through regular HTML file input 
    * This method will only be called if the target is not a cordova app.
-   * @param  {any} path
+   * @param  {any} nativeFilePath
    */
-  uploadFileViaNativeFilePath(path){
-    // TODO:  Once done implementing this feature on native device
-    //        Check if we still need the native file plugin. Otherwise we remove it to save space and performance.
+  uploadFileViaNativeFilePath(nativeFilePath){
+    this.isUploading = true;
+    this._attemptNativeFileUpload(nativeFilePath)
+      .then((downloadUrl: string) => {
+        this.isUploading = false;
+        this.value = downloadUrl;
+      })
+      .catch((err) => {
+        this.isUploading = false;
+        alert(err);
+      });
+  }
 
-    // Upload and process for progress
-    // this._awsService.uploadNativePath(this.prefix, path)
-    //   .then((uploadObservable) => {
-    //     this.processFileUpload(uploadObservable);
-    //   })
-    //   .catch((err) => {
-    //     alert(err);
-    //   });
+  /**
+   * Attempt native file upload
+   * @param nativeFilePath 
+   */
+  private _attemptNativeFileUpload(nativeFilePath){
+    return new Promise((resolve, reject) => {
+      // Resolve File Path on System 
+      this._nativeFile.resolveLocalFilesystemUrl(nativeFilePath).then((entry: Entry) => {
+        // Convert entry into File Entry which can output a JS File object
+        let fileEntry =  entry as FileEntry;
+
+        // Return a File object that represents the current state of the file that this FileEntry represents
+        fileEntry.file((file: any) => {
+
+            // Store File Details for later use
+            let fileName = file.name;
+            let fileType = file.type;
+            let fileLastModified = file.lastModifiedDate;
+
+            // Read File as Array Buffer, Convert to Blob, then to JS File
+            var reader = new FileReader();
+            reader.onloadend = (event: any) => {
+                var blob = new Blob([new Uint8Array(event.target.result)], { type: fileType });
+                var file: any = blob;
+                file.name = fileName;
+                file.lastModifiedDate = fileLastModified;
+
+                // Upload file to Firebase Storage
+                let storageRef = firebase.storage().ref();
+                let uploadRef = storageRef.child(`${this.bucketUrl}/${file.name}`);
+                uploadRef.put(file).then((snapshot) => {
+                  if(snapshot.state == "success"){
+                    resolve(snapshot.downloadURL);
+                  }
+                });
+            };
+            reader.readAsArrayBuffer(file);
+
+        }, (error) => {
+            reject("Unable to retrieve file properties: " + JSON.stringify(error));
+        });
+      }).catch(err => { 
+          reject("Error resolving file " + JSON.stringify(err));
+      });
+    });
   }
 
   /**
