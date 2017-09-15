fddb1f9e9e7a90a04d3b5e1fd91eb41d8dce6130
Khalid <khalid@bawes.net>
2017-09-15 17:43:56 +0300

    Setup firebase hosting

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Hello World Https
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+.onWrite(event => {
+    // Grab the current value of what was written to the Realtime Database.
+    const original = event.data.val();
+    console.log('Uppercasing', event.params.pushId, original);
+    const uppercase = original.toUpperCase();
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+    return event.data.ref.parent.child('uppercase').set(uppercase);
+});
